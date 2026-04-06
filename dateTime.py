285b5329bae40997f56c9c5c3ef86dd3a5d599a0
piyash9 <piyash@divint-it.net>
2026-04-06 17:59:45 +0600

    Added <dateTime.py> pratice with datetime libriary

---
+++ dateTime.py
@@ -0,0 +1,17 @@
+
+import datetime
+
+current_time = datetime.datetime.now()
+
+print(current_time)
+
+today = datetime.datetime.today()
+
+print(today)
+
+print(current_time.day)
+print(current_time.year)
+print(current_time.month)
+print(current_time.hour)
+print(current_time.month)
+print(current_time.second)
